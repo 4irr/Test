a407863dad26f369e6e664531d40962a3acc9436
4irr <chirva2015@list.ru>
2023-10-06 11:45:36 +0300

    Вынесение настройки аутентификации и Swagger конфигурации в методы расширения

--- API/Program.cs
+++ API/Program.cs
@@ -1,116 +1,40 @@
-using System.Reflection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using API;
 using API.Middleware;
 using Application;
 using Domain;
 using Persistence;
 using Security;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomJwtGenerator(builder.Configuration);
-builder.Services.AddControllers();
-
-builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
-    {
-        config.Password.RequiredLength = 4;
-        config.Password.RequireDigit = false;
-        config.Password.RequireNonAlphanumeric = false;
-        config.Password.RequireUppercase = false;
-    })
-    .AddEntityFrameworkStores<ApplicationContext>()
-    .AddSignInManager<SignInManager<AppUser>>();
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Events.OnRedirectToLogin = context =>
-    {
-        context.Response.StatusCode = 401;
-        return Task.CompletedTask;
-    };
-});
-
-builder.Services.AddAuthentication(config =>
-    {
-        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]!)),
-            ValidateAudience = false,
-            ValidateIssuer = false,
-        };
-    });
-
-builder.Services.AddSwaggerGen(config =>
-{
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    config.IncludeXmlComments(xmlPath);
-
-    config.AddSecurityDefinition("AuthToken",
-    new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "bearer",
-        Name = "Authorization",
-        Description = "Authorization token"
-    });
-
-    config.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "AuthToken"
-                }
-            },
-            new string[] { }
-        }
-    });
-
-    config.CustomOperationIds(apiDescription =>
-                apiDescription.TryGetMethodInfo(out MethodInfo methodInfo)
-                    ? methodInfo.Name
-                    : null);
-});
+builder.Services.AddCustomAuthentication(builder.Configuration);
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseCustomExceptionHandler();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 
 app.UseSwagger();
 app.UseSwaggerUI(config =>
 {
     config.RoutePrefix = string.Empty;
-    config.SwaggerEndpoint("swagger/v1/swagger.json", "Test Task API");
+    config.SwaggerEndpoint("swagger/Test Task API/swagger.json", "Test Task API");
 });
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
